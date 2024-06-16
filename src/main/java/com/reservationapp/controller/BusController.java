51d81e9cc596318fb6832851d96042cf83bce5f1
John Doe <john.doe@example.com>
2024-06-16 09:45:56 +0530

    git commit -am "initialchanges"

---
+++ src/main/java/com/reservationapp/controller/BusController.java
@@ -0,0 +1,110 @@
+package com.reservationapp.controller;
+
+import com.reservationapp.entity.Bus;
+import com.reservationapp.entity.Route;
+import com.reservationapp.entity.SubRoute;
+import com.reservationapp.payload.BusDto;
+import com.reservationapp.payload.SearchListOfBusesDto;
+import com.reservationapp.repository.BusRepository;
+import com.reservationapp.repository.RouteRepository;
+import com.reservationapp.repository.SubRouteRepository;
+import com.reservationapp.service.BusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.persistence.EntityManager;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/bus")
+public class BusController {
+    @Autowired
+    private BusService busService;
+    @Autowired
+    private RouteRepository routeRepository;
+    @Autowired
+    private BusRepository busRepository;
+    @Autowired
+    private SubRouteRepository subRouteRepository;
+
+
+    //http:localhost:8080/api/v1/bus/add
+    @PostMapping("/add")
+    public ResponseEntity<String> saveBus(@RequestBody BusDto busDto) throws ParseException {
+
+        Bus bus = busService.addBus(busDto);
+        return new ResponseEntity<>("Bus Detail Added", HttpStatus.CREATED);
+    }
+
+    //http:localhost:8080/api/v1/bus/add?fromLocation=&toLocation=&fromDate
+    @GetMapping
+    public List<SearchListOfBusesDto> getAllBuses(@RequestParam String fromLocation,
+                                                  @RequestParam String toLocation,
+                                                  @RequestParam String fromDate) {
+        List<Route> routes = routeRepository.findByFromLocationAndToLocationAndFromDate(fromLocation,
+                toLocation, fromDate);
+        List<SubRoute> subRoutes = subRouteRepository.findByFromLocationAndToLocationAndFromDate(fromLocation,
+                toLocation, fromDate);
+        List<SearchListOfBusesDto> buses = new ArrayList<>();
+        if(routes!=null){
+            for(Route route:routes){
+                Bus bus = busRepository.findById(route.getBusId()).get();
+                SearchListOfBusesDto searchListOfBusesDto = mapToSearchListOfBusesDto(bus, route);
+                buses.add(searchListOfBusesDto);
+            }
+            return buses;
+        }
+        if(subRoutes!=null){
+            for(SubRoute route:subRoutes){
+                Bus bus = busRepository.findById(route.getBusId()).get();
+                SearchListOfBusesDto searchListOfBusesDto = mapToSearchListOfBusesDto(bus, route);
+                buses.add(searchListOfBusesDto);
+            }
+            return buses;
+        }
+        return null;
+    }
+    SearchListOfBusesDto mapToSearchListOfBusesDto(Bus bus, Route route){
+        SearchListOfBusesDto searchListOfBusesDto = new SearchListOfBusesDto();
+        searchListOfBusesDto.setBusId(bus.getId());
+        searchListOfBusesDto.setBusNumber(bus.getBusNumber());
+        searchListOfBusesDto.setBusType(bus.getBusType());
+        searchListOfBusesDto.setPrice(bus.getPrice());
+        searchListOfBusesDto.setTotalSeat(bus.getTotalSeat());
+        searchListOfBusesDto.setAvailableSeat(bus.getAvailableSeat());
+
+        searchListOfBusesDto.setRouteId(route.getId());
+        searchListOfBusesDto.setFromLocation(route.getFromLocation());
+        searchListOfBusesDto.setToLocation(route.getToLocation());
+        searchListOfBusesDto.setFromDate(route.getFromDate());
+        searchListOfBusesDto.setToDate(route.getToDate());
+        searchListOfBusesDto.setTotalDuration(route.getTotalDuration());
+        searchListOfBusesDto.setFromTime(route.getFromTime());
+        searchListOfBusesDto.setToTime(route.getToTime());
+        return searchListOfBusesDto;
+    }
+    SearchListOfBusesDto mapToSearchListOfBusesDto(Bus bus, SubRoute route){
+        SearchListOfBusesDto searchListOfBusesDto = new SearchListOfBusesDto();
+        searchListOfBusesDto.setBusId(bus.getId());
+        searchListOfBusesDto.setBusNumber(bus.getBusNumber());
+        searchListOfBusesDto.setBusType(bus.getBusType());
+        searchListOfBusesDto.setPrice(bus.getPrice());
+        searchListOfBusesDto.setTotalSeat(bus.getTotalSeat());
+        searchListOfBusesDto.setAvailableSeat(bus.getAvailableSeat());
+
+        //searchListOfBusesDto.setRouteId(route.getId());
+        searchListOfBusesDto.setFromLocation(route.getFromLocation());
+        searchListOfBusesDto.setToLocation(route.getToLocation());
+        searchListOfBusesDto.setFromDate(route.getFromDate());
+        searchListOfBusesDto.setToDate(route.getToDate());
+        searchListOfBusesDto.setTotalDuration(route.getTotalDuration());
+        searchListOfBusesDto.setFromTime(route.getFromTime());
+        searchListOfBusesDto.setToTime(route.getToTime());
+        return searchListOfBusesDto;
+    }
+}
---
+++ src/main/java/com/reservationapp/util/ExcelGeneratorService.java
@@ -0,0 +1,50 @@
+package com.reservationapp.util;
+
+import com.reservationapp.entity.Passenger;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+@Service
+public class ExcelGeneratorService {
+    public byte[] generateExcelFile(List<Passenger> passengers) throws IOException {
+        // Create a new workbook
+        try (Workbook workbook = new XSSFWorkbook()) {
+            // Create a sheet
+            Sheet sheet = workbook.createSheet("Passenger Data");
+
+            // Create header row
+            Row headerRow = sheet.createRow(0);
+            String[] headers = {"ID", "First Name", "Last Name", "Email", "Mobile", "Bus ID", "Route ID"};
+            for (int i = 0; i < headers.length; i++) {
+                Cell cell = headerRow.createCell(i);
+                cell.setCellValue(headers[i]);
+            }
+
+            // Create data rows
+            int rowNum = 1;
+            for (Passenger passenger : passengers) {
+                Row row = sheet.createRow(rowNum++);
+                row.createCell(0).setCellValue(passenger.getId());
+                row.createCell(1).setCellValue(passenger.getFirstName());
+                row.createCell(2).setCellValue(passenger.getLastName());
+                row.createCell(3).setCellValue(passenger.getEmail());
+                row.createCell(4).setCellValue(passenger.getMobile());
+                row.createCell(5).setCellValue(passenger.getBusId());
+                row.createCell(6).setCellValue(passenger.getRouteId());
+            }
+
+            // Write the workbook to a file
+            ByteArrayOutputStream outputStream= new ByteArrayOutputStream();
+            workbook.write(outputStream) ;
+            return outputStream.toByteArray();
+        }
+    }
+}
